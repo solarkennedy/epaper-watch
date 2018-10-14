93d081ce704dffc1bc9a57dfff4c9bec150880bd
Kyle Anderson <kyle@xkyle.com>
2018-10-14 16:44:28 -0700

    real printing and csv cleanup

--- csv2lit.ino.py
+++ csv2lit.ino.py
@@ -32,9 +32,71 @@ def strip_bad(s):
     return s.replace('\n', ' ')
 
 
+def print_words_so_far(words_so_far, cursor, font, fg, bg):
+    words = ' '.join(words_so_far)
+    return f'paint.Paint_DrawString_EN({cursor[0]}, {cursor[1]}, "{words}", &Font{font}, {fg}, {bg});'
+    #return f'paint.Paint_DrawString_EN({cursor[0]}, {cursor[1]}, "{' '.join(words_so_far)}", &Font{font}, {fg}, {bg});'
+
+
+def quote_to_print_lines(row, display_width=212, display_height=101, margin=5, font=12):
+    char_width=7
+    code = []
+    words_so_far = []
+    cursor= (margin, margin)
+    quote = strip_bad(escape(row['quote'])).replace(row['time_string'], "TIMESTRING")
+    timestring_cursor = None
+    for word in quote.split():
+        print(f"DEBUG: Operating on {word}")
+        if "TIMESTRING" in word:
+            print(f"DEBUG: Wrorking on the timestring")
+            if cursor[0] + len(row['time_string'])*char_width <= display_width - margin:
+                timestring_cursor = cursor
+                cursor = (cursor[0] + len(row['time_string'])*char_width, cursor[1])
+            else:
+                # We won't fit on this row, let's flush the buffer to make room
+                # and mark where the cursor would go next
+                drawing_cursor = (margin, cursor[1])
+                code.append(print_words_so_far(words_so_far, drawing_cursor, font, "WHITE", "BLACK"))
+                words_so_far = []
+                cursor = (margin, cursor[1] + font)
+                timestring_cursor = cursor
+                cursor = (cursor[0] + len(row['time_string'])*char_width, cursor[1])
+            
+
+        if cursor[0] + len(word)*char_width <= display_width - margin:
+            # If we can fit, let's just append the word
+            words_so_far.append(word)
+            cursor = (cursor[0] + len(word)*char_width, cursor[1])
+        else:
+            # We don't fit, so let's flush
+            drawing_cursor = (margin, cursor[1])
+            code.append(print_words_so_far(words_so_far, drawing_cursor, font, "WHITE", "BLACK"))
+            words_so_far = []
+            cursor = (margin, cursor[1] + font)
+
+    # Now we print the time string, which is highlighted
+    assert timestring_cursor, quote
+    code.append(print_words_so_far([row['time_string']], timestring_cursor, font, "BLACK", "WHITE"))
+
+    # Now we print the attribution, which is right justified and properly formatted
+    attribution_string = f"- {row['author'].strip()}, {row['book']}"
+    x = display_width - margin - len(attribution_string)*char_width
+    if not x > margin:
+        print(f"att string too long: '{attribution_string}', needs an x of {x}")
+    else: 
+        y =  display_height - margin - font
+        cursor = (x, y)
+        code.append(print_words_so_far([attribution_string], cursor, font, "WHITE", "BLACK"))
+
+    return code
+
+
+
 def print_code_for_a_single_quote(row, indent):
     full_quote = row_to_full_quote(row)
     print(f'{" "*indent}Serial.println(F("{full_quote}"));')
+    for line in quote_to_print_lines(row):
+        print(f'{" "*indent}{line}')
 #    print(f'{" "*indent}paint.Paint_DrawString_EN(0, 0, "{full_quote}", &Font12, WHITE, BLACK);')
 
 
