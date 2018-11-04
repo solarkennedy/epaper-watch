106d29dc9fdb843fe3ee856dc2b4b888205ffe01
Kyle Anderson <kyle@xkyle.com>
2018-11-04 12:26:48 -0800

    Some power management

--- epaper-watch.ino
+++ epaper-watch.ino
@@ -1,14 +1,18 @@
 
 
 void setup(void) {
-
   Serial.begin(115200);
-  delay(50);
+  Serial.setTimeout(2000);
+  while (!Serial) { }
   setupClock();
-
+  getWakeupReason();
+  Serial.println("Going into deep sleep for 20 seconds");
+  ESP.deepSleep(20e6);
 }
 
 
 void loop(void) {
   handleTimeEvents();
+  Serial.println("Going into deep sleep for for the second time for 40 seconds");
+  ESP.deepSleep(40e6);
 }
--- time.ino
+++ time.ino
@@ -1,5 +1,40 @@
 #include <EEPROM.h>
 #include <ezTime.h>
+
+extern "C" {
+#include "user_interface.h" // this is for the RTC memory read/write functions
+}
+#define RTCMEMORYSTART 66
+#define RTCMEMORYLEN 125
+
+
+typedef struct {
+  int magicNumber;
+  int valueCounter;
+} rtcManagementStruc;
+
+rtcManagementStruc rtcManagement;
+
+
+void getWakeupReason() {
+
+  rst_info *rsti;
+  rsti = ESP.getResetInfoPtr();
+
+  switch (rsti->reason) {
+    case 5:
+      Serial.println(" from RTC-RESET (ResetInfo.reason = 5)");
+      break;
+    case 6:
+      Serial.println(" from POWER-UP (ResetInfo.reason = 6)");
+      break;
+  }
+  Serial.println("I'm awake.");
+  system_rtc_mem_read(64, &rtcManagement, 8);
+
+}
+
+
 void handleTimeEvents() {
   events();
 }
