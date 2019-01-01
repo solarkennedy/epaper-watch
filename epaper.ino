971f69c1bc47cfd1693f17840f892245f6596bd2
Kyle Anderson <kyle@xkyle.com>
2018-12-31 16:18:41 -0800

    Working battery meter

--- epaper.ino
+++ epaper.ino
@@ -15,6 +15,9 @@
 GxIO_Class io(SPI, CS_PIN, DC_PIN, RST_PIN);
 GxEPD_Class display(io, GPIO_PIN_RESET, BUSY_PIN);
 
+const float VOLTAGE_DIVIDER_RATIO = 5.7;
+const float V_REF = 1.0;
+
 
 void setupEpaper() {
   delay(1000);
@@ -29,7 +32,7 @@ void setupEpaper() {
   Serial.println("show font");
   delay(1000);
   showFont();
-  drawBatteryMeter(50);
+  drawBatteryMeter(readBatteryLevel());
   display.fillRect(0, 0, 8, 8, GxEPD_BLACK);
   display.fillRect(display.width() - 18, 0, 16, 16, GxEPD_BLACK);
   display.fillRect(display.width() - 25, display.height() - 25, 24, 24, GxEPD_BLACK);
@@ -54,11 +57,27 @@ void showFont()
   display.println("PQRSTUVWXYZ[\\]^_");
   display.println("`abcdefghijklmno");
   display.println("pqrstuvwxyz{|}~ ");
-
 }
 
-void drawBatteryMeter(int percent) {
+uint8_t readBatteryLevel() {
+  float voltage = analogRead(A0) * VOLTAGE_DIVIDER_RATIO / 1024 * 0.980716253;
+  uint8_t percent = sigmoidal(voltage, 0, 4.2);
+  Serial.print("Battery Percent: ");
+  Serial.print(percent);
+  Serial.print(" (");
+  Serial.print(voltage);
+  Serial.println(" volts)");
+  return percent;
+}
+
+void drawBatteryMeter(uint8_t percent) {
   int h = round(display.height() * percent / 100);
   display.fillRect(display.width() - 1, display.height() - h , 1, h, GxEPD_BLACK);
   //display.updateWindow(display.width() - 1, 0 , display.width(), display.height());
+}
+
+
+static inline uint8_t sigmoidal(uint16_t voltage, uint16_t minVoltage, uint16_t maxVoltage) {
+  uint8_t result = 105 - (105 / (1 + pow(1.724 * (voltage - minVoltage) / (maxVoltage - minVoltage), 5.5)));
+  return result >= 100 ? 100 : result;
 }
